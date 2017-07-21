1c23949b49e6240f99a9f41e17c2dc5b92bf6465
Erik Zorn <erik.zorn44@gmail.com>
2017-07-20 21:44:32 -0400

    Chart.js initial implementation

--- src/providers/backand/backand.ts
+++ src/providers/backand/backand.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class BackandProvider {
-  auth_token: {header_name: string, header_value: string} = {header_name: 'e5bb81cf-6bb9-4974-ae20-bc71176163d5', header_value: '2d6893c2-a1fa-4a73-b745-9b1ac1789a87'};
+  auth_token: {header_name: string, header_value: string} = {header_name: 'AnonymousToken', header_value: 'e5bb81cf-6bb9-4974-ae20-bc71176163d5'};
   api_url: string = 'https://api.backand.com'
   app_name: string = 'budget5897'
 
