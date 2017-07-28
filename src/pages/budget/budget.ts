d468f244664fae4b073b318a44cbcae58fdc2f6b
Erik Zorn <erik.zorn44@gmail.com>
2017-07-27 20:28:07 -0400

    Pulling data from backand into charts

--- src/pages/budget/budget.ts
+++ src/pages/budget/budget.ts
@@ -2,6 +2,8 @@ import { Component,ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Chart } from 'chart.js';
 
+import { BackandProvider } from '../../providers/backand/backand';
+
 /**
  * Generated class for the BudgetPage page.
  *
@@ -22,19 +24,83 @@ export class BudgetPage {
   barChart: any;
   lineChart: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  transactions = []
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public backandService: BackandProvider) {
+  	//this.createTotalExpenseDict()
+  	
   }
 
+  // private loadTransactions() {
+  // 	this.backandService.getTransactions()
+  // 	.subscribe(
+  // 		data => {
+  // 			console.log(data.data);
+  // 			this.transactions = data.data;
+  // 		},
+  // 		err => this.logError(err)
+  // 		);
+  // }
+
+  private loadTransactions(callback) {
+  	// dictionary for storing total expenses by category
+
+  	this.backandService.getTransactions()
+  	.subscribe(data => {
+  		// populate array with all transactions
+  		this.transactions = data.data;
+	  	var transactionDict = callback(this.transactions);
+	  	var labelArr = Object.keys(transactionDict);
+	  	var dataArr = [];
+
+	  	for (var i = 0; i < labelArr.length; i++) {
+	  		dataArr[i] = transactionDict[labelArr[i]];
+	  	}
+	  	this.makeCharts(labelArr, dataArr);
+	  });
+  }
+
+  public logError(err: TemplateStringsArray) {
+    console.error('Error: ' + err);
+  }
+
+  // public getCategories(transactions) {
+  // 	let totalExpenseDict = {};
+  // 	for (var i = 0; i < transactions.length; i++) {
+	 //  		if (transactions[i].category in totalExpenseDict) {
+	 //  			totalExpenseDict[transactions[i].category] = totalExpenseDict[transactions[i].category] + transactions[i].amount;
+	 //  		}
+	 //  		else {
+	 //  			totalExpenseDict[transactions[i].category] = transactions[i].amount;
+	 //  		}
+	 // }
+	 // console.log(totalExpenseDict)
+  // }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad BudgetPage');
-    
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+  	this.loadTransactions(transactions => {
+  		let totalExpenseDict = {};
+	  	for (var i = 0; i < transactions.length; i++) {
+		  		if (transactions[i].category in totalExpenseDict) {
+		  			totalExpenseDict[transactions[i].category] = totalExpenseDict[transactions[i].category] + transactions[i].amount;
+		  		}
+		  		else {
+		  			totalExpenseDict[transactions[i].category] = transactions[i].amount;
+		  		}
+		 }
+		console.log(totalExpenseDict)
+  		return totalExpenseDict;
+  	
+  	});
+  }
+  public makeCharts(labelArr, dataArr) {
+  	this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
         type: 'doughnut',
         data: {
-            labels: ["Entertainment", "Food", "Groceries", "Gas", "Alc/Drugs", "Misc."],
+            labels: labelArr,
             datasets: [{
                 label: '# of Votes',
-                data: [4, 3, 4, 4, 30, 3],
+                data: dataArr,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -123,6 +189,5 @@ export class BudgetPage {
             ]
         }
     });
-  }
-
+   }s
 }
